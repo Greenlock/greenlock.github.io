c77d745c43108043f1130801fc39e79eddc409a2
Greenlock <pokedoxysm@gmail.com>
2016-05-18 13:17:16 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 function onLoad() {
 	wcInitialize();
-	wcPrintLine("====  Welcome to greenlock.co  ====");
-	wcPrintCharacter(">");
-	wcReadLine(function (e) { alert(e.length.toString()); wcReadLine(function (e) { alert(e.length.toString()); }); });
+        wcPrintCharacter(">");
 }
