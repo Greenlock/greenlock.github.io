6679d6745831f11ef7c98f25c8199a840eba7b12
Greenlock <pokedoxysm@gmail.com>
2016-05-19 11:21:23 -0400

    Implemented the basic shell with 'echo'

--- shell.js
+++ shell.js
@@ -1,4 +1,89 @@
 function entrypoint() {
     terminal.initialize();
-    terminal.printLine("Under construction.");
+
+    terminal.printLine("Welcome to greenlock.co! This website is a command-line terminal.");
+    terminal.printLine("Type 'help' for more information.");
+
+    repl();
+}
+
+function repl() {
+    terminal.printLine("");
+    terminal.print("user />");
+    terminal.readLine(function (command) {
+        var args = parseCommand(command);
+        if (args[0].toLowerCase() == "help") {
+            terminal.printLine("This website is a very (veery) simple implementation of a Bash-esque command");
+            terminal.printLine("line. There is currently no user or filesystem support. This is pretty much");
+            terminal.printLine("a barebones system right now.");
+            terminal.printLine("");
+            terminal.printLine("As of now, available commands are:");
+            terminal.printLine("echo  --  Prints a given line of text.");
+        } else if (args[0].toLowerCase() == "echo") {
+            var remainingArgs = argsFrom(1, args);
+            var echo = "";
+            for (var i = 0; i < remainingArgs.length; i++) {
+                echo += (i == 0 ? "" : " ") + remainingArgs[i];
+            }
+            terminal.printLine(echo);
+        } else {
+            terminal.printLine(args[0] + " is not a known command.");
+        }
+        setTimeout(repl, 0);
+    });
+}
+
+function parseCommand(command) {
+    var args = [];
+    var currentToken = "";
+    var isLiteral = false;
+    for (var i = 0; i < command.length; i++) {
+        var c = command.charAt(i);
+        if (isLiteral) {
+            if (c == "\"") {
+                currentToken += "\"";
+                args.push(eval(currentToken));
+                isLiteral = false;
+            } else if (c == "\\") {
+                currentToken += "\\";
+                i++;
+                currentToken += command.charAt(i);
+            } else {
+                currentToken += c;
+            }
+        } else {
+            if (c == "\"") {
+                if (currentToken.length > 0) {
+                    args.push(currentToken);
+                    currentToken = "";
+                }
+                currentToken += "\"";
+                isLiteral = true;
+            } else if (c == " " || c == "\t") {
+                if (currentToken.length > 0) {
+                    args.push(currentToken);
+                    currentToken = "";
+                }
+            } else {
+                currentToken += c;
+            }
+        }
+    }
+    if (currentToken.length > 0) {
+        if (currentToken.startsWith("\"")) {
+            currentToken += "\"";
+            args.push(eval(currentToken));
+        } else {
+            args.push(currentToken);
+        }
+    }
+    return args;
+}
+
+function argsFrom(index, args) {
+    var returnList = [];
+    for (var i = index; i < args.length; i++) {
+        returnList.push(args[i]);
+    }
+    return returnList;
 }
--- terminal.js
+++ terminal.js
@@ -13,6 +13,8 @@
         WC_READ_LINE = 1,
         WC_READ_KEY = 2;
 
+    var keycodes = ["", "", "", "CANCEL", "", "", "HELP", "", "BACK_SPACE", "TAB", "", "", "CLEAR", "ENTER", "ENTER_SPECIAL", "", "SHIFT", "CONTROL", "ALT", "PAUSE", "CAPS_LOCK", "KANA", "EISU", "JUNJA", "FINAL", "HANJA", "", "ESCAPE", "CONVERT", "NONCONVERT", "ACCEPT", "MODECHANGE", "SPACE", "PAGE_UP", "PAGE_DOWN", "END", "HOME", "LEFT", "UP", "RIGHT", "DOWN", "SELECT", "PRINT", "EXECUTE", "PRINTSCREEN", "INSERT", "DELETE", "", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "COLON", "SEMICOLON", "LESS_THAN", "EQUALS", "GREATER_THAN", "QUESTION_MARK", "AT", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "OS_KEY", "", "CONTEXT_MENU", "", "SLEEP", "NUMPAD0", "NUMPAD1", "NUMPAD2", "NUMPAD3", "NUMPAD4", "NUMPAD5", "NUMPAD6", "NUMPAD7", "NUMPAD8", "NUMPAD9", "MULTIPLY", "ADD", "SEPARATOR", "SUBTRACT", "DECIMAL", "DIVIDE", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13", "F14", "F15", "F16", "F17", "F18", "F19", "F20", "F21", "F22", "F23", "F24", "", "", "", "", "", "", "", "", "NUM_LOCK", "SCROLL_LOCK", "WIN_OEM_FJ_JISHO", "WIN_OEM_FJ_MASSHOU", "WIN_OEM_FJ_TOUROKU", "WIN_OEM_FJ_LOYA", "WIN_OEM_FJ_ROYA", "", "", "", "", "", "", "", "", "", "CIRCUMFLEX", "EXCLAMATION", "DOUBLE_QUOTE", "HASH", "DOLLAR", "PERCENT", "AMPERSAND", "UNDERSCORE", "OPEN_PAREN", "CLOSE_PAREN", "ASTERISK", "PLUS", "PIPE", "HYPHEN_MINUS", "OPEN_CURLY_BRACKET", "CLOSE_CURLY_BRACKET", "TILDE", "", "", "", "", "VOLUME_MUTE", "VOLUME_DOWN", "VOLUME_UP", "", "", "SEMICOLON", "EQUALS", "COMMA", "MINUS", "PERIOD", "SLASH", "BACK_QUOTE", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "OPEN_BRACKET", "BACK_SLASH", "CLOSE_BRACKET", "QUOTE", "", "META", "ALTGR", "", "WIN_ICO_HELP", "WIN_ICO_00", "", "WIN_ICO_CLEAR", "", "", "WIN_OEM_RESET", "WIN_OEM_JUMP", "WIN_OEM_PA1", "WIN_OEM_PA2", "WIN_OEM_PA3", "WIN_OEM_WSCTRL", "WIN_OEM_CUSEL", "WIN_OEM_ATTN", "WIN_OEM_FINISH", "WIN_OEM_COPY", "WIN_OEM_AUTO", "WIN_OEM_ENLW", "WIN_OEM_BACKTAB", "ATTN", "CRSEL", "EXSEL", "EREOF", "PLAY", "ZOOM", "", "PA1", "WIN_OEM_CLEAR", ""];
+
 
     terminal.initialize = function () {
         var body = $("body");
@@ -49,9 +51,13 @@
         container.append(stdoutContainer);
         container.append(stdin);
         body.append(container);
+
+        var style = $("<style></style>");
+        style.html("#stdin:focus { outline: none; }");
+        $("head").append(style);
     };
 
-    terminal.printChar = function(c) {
+    function printChar(c) {
         if (terminal.cursorLeft >= 80) terminal.cursorLeft = 79;
         if (terminal.cursorLeft < 0) terminal.cursorLeft = 0;
         if (terminal.cursorTop < 0) terminal.cursorTop = 0;
@@ -87,57 +93,64 @@
                 terminal.cursorLeft = 0;
                 terminal.cursorTop++;
             } else {
-                terminal.printChar(c);
+                printChar(c);
             }
         }
+
+        location.href = "#";
+        location.href = "#stdout-" + terminal.cursorTop.toString();
     };
 
     terminal.printLine = function(line) {
         terminal.print(line + "\n");
     };
 
+    terminal.clearScreen = function() {
+        $("#stdout-container").html("");
+    };
+
     terminal.readLine = function(callback) {
-        wcReadMode = WC_READ_LINE;
-        wcReadCallback = callback;
+        readMode = WC_READ_LINE;
+        readCallback = callback;
         var stdinObject = $("#stdin");
-        stdinObject.css("left", (wcCursorLeft * 9.601).toString() + "px");
-        stdinObject.css("top", ((wcCursorTop * 18) - 1).toString() + "px");
-        stdinObject.css("width", (770 - (wcCursorLeft * 9.601)).toString() + "px");
+        stdinObject.css("left", (terminal.cursorLeft * 9.601).toString() + "px");
+        stdinObject.css("top", ((terminal.cursorTop * 18) - 1).toString() + "px");
+        stdinObject.css("width", (770 - (terminal.cursorLeft * 9.601)).toString() + "px");
         stdinObject.css("visibility", "visible");
+        stdinObject.focus();
     };
 
     terminal.readKey = function(callback) {
-        wcReadMode = WC_READ_KEY;
-        wcReadCallback = callback;
+        readMode = WC_READ_KEY;
+        readCallback = callback;
         var stdinObject = $("#stdin");
-        stdinObject.css("left", (wcCursorLeft * 9.601).toString() + "px");
-        stdinObject.css("top", ((wcCursorTop * 18) - 1).toString() + "px");
-        stdinObject.css("width", "9.6px");
+        stdinObject.css("left", (terminal.cursorLeft * 9.601).toString() + "px");
+        stdinObject.css("top", ((terminal.cursorTop * 18) - 1).toString() + "px");
+        stdinObject.css("width", "10px");
         stdinObject.css("visibility", "visible");
+        stdinObject.focus();
     };
 
     function handleKeyDown(event) {
-	    if (wcReadMode == WC_READ_LINE && event.keyCode == 13) {
-	        intputValue = _.unescape($("#stdin").val());
-	        wcReadMode = WC_NONE;
+	    if (readMode == WC_READ_LINE && event.keyCode == 13) {
+	        inputValue = _.unescape($("#stdin").val());
+	        readMode = WC_NONE;
 	        $("#stdin").val("");
 	        $("#stdin").css("visibility", "hidden");
-	        wcCursorLeft = 0;
-	        wcCursorTop++;
-		    if (wcReadCallback != null) {
-		        wcReadCallback(intputValue);
-			    wcReadCallback = null;
+	        terminal.printLine(inputValue);
+		    if (readCallback != null) {
+		        readCallback(inputValue);
+			    readCallback = null;
 		    }
 		    return false;
-	    } else if (wcReadMode == WC_READ_KEY) {
-	        wcReadMode = WC_NONE;
+	    } else if (readMode == WC_READ_KEY) {
+	        readMode = WC_NONE;
 	        $("#stdin").val("");
 	        $("#stdin").css("visibility", "hidden");
-	        wcCursorLeft = 0;
-	        wcCursorTop++;
-	        if (wcReadCallback != null) {
-	            wcReadCallback(event.keyCode);
-	            wcReadCallback = null;
+	        terminal.print(keycodes[event.keyCode].length > 1 ? "^" : keycodes[event.keyCode]);
+	        if (readCallback != null) {
+	            readCallback(event.keyCode);
+	            readCallback = null;
 	        }
 	        return false;
 	    } else {
